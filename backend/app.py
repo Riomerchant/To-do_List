594c354261cc73bffd0fb0d8539e87d93403ea11
Ansh  jain <135792701+Riomerchant@users.noreply.github.com>
2025-07-25 22:55:26 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,8 @@ import json
 import os
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://to-do-list-vx26.vercel.app"}})
+CORS(app, resources={r"/*": {"origins": "https://to-do-list-vx26.vercel.app"}}, supports_credentials=True)
+
 
 DATA_FILE = 'db.json'
 
